d6bcd532328bbedda5d39726ea6cdcc383eca08c
najar1412 <rory.jarrel@gmail.com>
2018-02-10 04:59:59 -0500

    blocked out ui

--- glanceapp/app.py
+++ glanceapp/app.py
@@ -1,19 +1,25 @@
-import sys
-# sys.path.append('D:/glanceapp/glanceapp/')
+"""
+glanceapp
+3dsmax 2018
+Contains allthe code
+"""
 
-from PySide import QtGui
-from PySide import QtCore
-import urllib2
-import MaxPlus
+import sys
 import json
 import base64
+import urllib2
+
+import MaxPlus
+from PySide2 import QtWidgets
+from PySide2 import QtGui
+from PySide2 import QtCore
 
 
 class Config():
-    username = ''
-    password = ''
-    entry_point = ''
-    storage_url = ''
+    username = 'admin'
+    password = 'admin'
+    entry_point = 'http://34.236.238.212:5050/glance/v2/items'
+    storage_url = 'https://s3.amazonaws.com/vhdevglancestore/'
 
 
 class GlanceLib():
@@ -23,7 +29,6 @@ class GlanceLib():
 
     def query(self, string_query):
         build_url_with_params = Config().entry_point + '?'+ 'query=' + string_query
-
         request = urllib2.Request(build_url_with_params)
         base64string = base64.b64encode('{}:{}'.format(Config().username, Config().password))
         request.add_header("Authorization", "Basic {}".format(base64string))
@@ -36,66 +41,92 @@ class GlanceLib():
             return []
 
 
-    def thumb_widget(self, image_name):
+    def thumb_widget(self, image_name, column=0):
         url = Config().storage_url + image_name
 
         data = urllib2.urlopen(url).read()
         image = QtGui.QImage()
         image.loadFromData(data)
 
-        lbl = QtGui.QLabel()
+        lbl = QtWidgets.QLabel()
         lbl.setPixmap(QtGui.QPixmap(image))
-        lbl2 = QtGui.QLabel()
-        lbl2.setText('View | Download')
+        
+        if column == 0:
+            self.context.addWidget(lbl)
+            
+        else:
+            self.context.addWidget(lbl, 0, 1)
 
-        self.context.addWidget(lbl)
-        self.context.addWidget(lbl2)
 
-
-class example(QtGui.QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        super(example, self).__init__()
-        self.setFixedWidth(500)
-        self.setFixedHeight(800)
+        super(MainWindow, self).__init__(parent)
+        self.construct_ui()
 
-        self.query_text = QtGui.QLineEdit()
 
-        grid = QtGui.QGridLayout()
-        self.widget = QtGui.QWidget()
-        self.layout = QtGui.QGridLayout(self.widget)
+    def construct_ui(self):
+        self.setWindowTitle('glanceapp')
 
-        self.layout.addWidget(self.query_text)
+        # main widget
+        main_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(main_widget)
 
-        btn = QtGui.QPushButton("Search...")
-        self.layout.addWidget(btn)
+        # layout initialize
+        self.g_layout = QtWidgets.QVBoxLayout()
+        self.layout_query = QtWidgets.QVBoxLayout()
+        self.layout = QtWidgets.QGridLayout()
+        main_widget.setLayout(self.g_layout)
+
+        # Add Widgets
+        self.query_input = QtWidgets.QLineEdit()
+        self.layout_query.addWidget(self.query_input)
+        
+        btn = QtWidgets.QPushButton("Search...")
+        self.layout_query.addWidget(btn)
         btn.clicked.connect(self.buttonClicked)
+        
+        self.build_columns()
 
-        self.scroll = QtGui.QScrollArea()
-        self.scroll.setWidgetResizable(True)
-        self.scroll.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        self.scroll.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        # global layout setting
+        self.g_layout.addLayout(self.layout_query)
+        self.g_layout.addLayout(self.layout)
 
-        self.scroll.setWidget(self.widget)
 
-        grid.addWidget(self.scroll, 3, 0)
+    def build_columns(self, items=None, amount=10):
+        if items:
+            count = 0
+            for item in items:
+                if count < amount:
+                    if count % 2 == 0:
+                        GlanceLib(self.layout).thumb_widget(item['item_thumb'])
 
-        self.setLayout(grid)
+                    else:
+                        GlanceLib(self.layout).thumb_widget(item['item_thumb'], column=1)
+                    
+                    count += 1
+        
+        else:
+            for index, _ in enumerate(range(8)):
+                if index % 2 == 0:
+                    GlanceLib(self.layout).thumb_widget('001_ftp1sQ_thumbnail.jpg')
+                    
+                else:
+                    GlanceLib(self.layout).thumb_widget('001_ftp1sQ_thumbnail.jpg', column=1)
 
 
     def buttonClicked(self):
-        all_items = GlanceLib(self.layout).query(self.query_text.text())
+        for i in reversed(range(self.layout.count())): 
+            self.layout.itemAt(i).widget().setParent(None)
+        self.layout = QtWidgets.QGridLayout()
+        self.g_layout.addLayout(self.layout)
 
-        count = 0
-
-        for item in all_items:
-            if count < 10:
-                GlanceLib(self.layout).thumb_widget(item['item_thumb'])
-                count += 1
-
-
+        all_items = GlanceLib(self.layout).query(self.query_input.text())
+        self.build_columns(items=all_items)
+        
+        
 if __name__ == '__main__':
-    app = QtGui.QApplication.instance()
-    dialog = example()
-    dialog.show()
+    app = QtWidgets.qApp
+    GUI = MainWindow()
+    GUI.show()
 
     sys.exit(app.exec_())
