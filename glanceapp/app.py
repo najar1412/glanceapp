96a025eeaf38344ea7f884a09c4b61887691de2f
najar1412 <najar1412@users.noreply.github.com>
2018-02-10 05:02:02 -0500

    Update app.py

--- glanceapp/app.py
+++ glanceapp/app.py
@@ -16,10 +16,10 @@ from PySide2 import QtCore
 
 
 class Config():
-    username = 'admin'
-    password = 'admin'
-    entry_point = 'http://34.236.238.212:5050/glance/v2/items'
-    storage_url = 'https://s3.amazonaws.com/vhdevglancestore/'
+    username = ''
+    password = ''
+    entry_point = ''
+    storage_url = ''
 
 
 class GlanceLib():
